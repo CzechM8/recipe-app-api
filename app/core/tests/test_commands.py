e4b9de2fdfe71266fcce00fb2c6bb2ecacb41979
Les Hardin <les.hardin@gentivahs.com>
2024-01-10 15:57:21 -0500

    Fixed linting settings

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -15,7 +15,6 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
   """ Test commands. """
 
-
   def test_wait_for_db_ready(self, patched_check):
     """ Test waiting for database to become ready. """
     patched_check.return_value = True
@@ -28,7 +27,7 @@ class CommandTests(SimpleTestCase):
   def test_wait_for_db_delay(self, patched_sleep, patched_check):
     """ Test waiting for database when getting OperationalError."""
     patched_check.side_effect = [Psycopg2Error] * 2 + \
-    [OperationalError] * 3 + [True]
+        [OperationalError] * 3 + [True]
 
     call_command('wait_for_db')
 
